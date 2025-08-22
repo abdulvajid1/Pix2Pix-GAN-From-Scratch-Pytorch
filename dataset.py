4ec4b6f06d439d39a4d78d5f3e73f8f661c5b7e6
abdul vajid <vajidca444@gmail.com>
2025-08-22 11:26:16 +0530

    final commit

--- dataset.py
+++ dataset.py
@@ -9,7 +9,6 @@ class MapDataset(Dataset):
         super().__init__()
         self.root_dir = root_dir
         self.list_files = os.listdir(self.root_dir)
-        print(self.list_files)
     
     def __len__(self):
         return len(self.list_files)
@@ -19,15 +18,14 @@ class MapDataset(Dataset):
         img_path = os.path.join(self.root_dir, image_file)
         image = np.array(Image.open(img_path))
         
-        print(f"Image size before splitting {image.shape}")
         
-        input_img = image[:, :600, :] # split the image input and target to image (1200, height) -> (600, h) , (600, h)
-        target_img = image[:, 600:, :]
-        
-        print(f"Image size after splitting {input_img.shape}")
-        
+        input_img = image[:, :256, :] # split the image input and target to image (1200, height) -> (600, h) , (600, h)
+        target_img = image[:, 256:, :]
+     
         augmentation = config.both_transform(image=input_img, image0=target_img)
         input_img, target_img = augmentation['image'], augmentation['image0']
         
         input_img = config.tranform_input(image=input_img)['image']
         target_img = config.tranform_target(image=target_img)['image']
+        
+        return input_img, target_img
--- train.py
+++ train.py
@@ -12,15 +12,24 @@ from discriminator_model import Discriminator
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from utils import save_checkpoint, save_some_samples, load_checkpoint
+from torch.utils.tensorboard import SummaryWriter
 
-def train_step(generator, discriminator, optim_gen, optim_desc, g_scaler, d_scaler, train_loader, bce_loss, l1_loss):
+writer = SummaryWriter(log_dir="runs/gan_experiment")
+
+def train_step(generator, discriminator, optim_gen, optim_desc, g_scaler, d_scaler, train_loader, bce_loss, l1_loss, val_loader):
     loop = tqdm(train_loader, leave=True)
     
     for idx, (x, y) in enumerate(loop):
-        x.to(config.DEVICE)
-        y.to(config.DEVICE)
+        x = x.to(config.DEVICE)
+        y = y.to(config.DEVICE)
         
-        with torch.cuda.amp.autocast():
+        # print('train loop 1 is ok')
+        # print("input =>",x.dtype)
+        # print("y dtype =>",y.dtype)
+        # print("input shape", x.shape)
+        # print("y shape", y.shape)
+        
+        with torch.amp.autocast(device_type=config.DEVICE):
             y_fake = generator(x)
             D_real = discriminator(x, y)
             D_fake = discriminator(x, y_fake.detach())
@@ -34,7 +43,7 @@ def train_step(generator, discriminator, optim_gen, optim_desc, g_scaler, d_scal
         d_scaler.update()
         
         
-        with torch.cuda.amp.autocast():
+        with torch.amp.autocast(device_type=config.DEVICE):
             y_fake = generator(x)
             D_fake = discriminator(x, y_fake)
             G_fake_loss = bce_loss(D_fake, torch.ones_like(D_fake))
@@ -46,6 +55,15 @@ def train_step(generator, discriminator, optim_gen, optim_desc, g_scaler, d_scal
         g_scaler.step(optim_gen)
         g_scaler.update()
         
+        if idx % 50 == 0:
+            save_some_samples(generator, val_loader, idx, folder='eval_files')
+        
+        loop.set_postfix({
+        "D_loss": D_loss.item(),
+        "G_loss": G_loss.item()
+        })
+        
+    return D_loss.item(), G_loss.item()
         
 
 def main():
@@ -57,27 +75,32 @@ def main():
     l1_loss = nn.L1Loss()
     
     if config.LOAD_MODEL:
+        print('Loadin Checkpoint')
         load_checkpoint(config.GEN_CHECKPOINT_PATH, generator, optim_gen, lr=config.LR)
         load_checkpoint(config.DESC_CHECKPOINT_PATH, discriminator, optim_desc, lr=config.LR)
         
     train_dataset = MapDataset(root_dir='dataset/train')
-    train_loader = DataLoader(train_dataset, batch_size=config.BATCH_SIZE, num_workers=config.NUM_WORKERS, shuffle=True,)
-    val_dataset = MapDataset(root_dir='dataset/test')
+    train_loader = DataLoader(train_dataset, batch_size=config.BATCH_SIZE, num_workers=config.NUM_WORKERS, shuffle=True)
+    
+    val_dataset = MapDataset(root_dir='dataset/val')
     val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
     
     
-    g_scaler = torch.cuda.amp.GradScaler()
-    d_scaler = torch.cuda.amp.GradScaler()
+    g_scaler = torch.amp.GradScaler()
+    d_scaler = torch.amp.GradScaler()
     
     
     for epoch in range(config.NUM_EPOC):
-        train_step(generator, discriminator, optim_gen, optim_desc, g_scaler, d_scaler, train_loader, bce_loss, l1_loss)
+        D_loss, G_loss = train_step(generator, discriminator, optim_gen, optim_desc, g_scaler, d_scaler, train_loader, bce_loss, l1_loss, val_loader)
+        if writer:
+            writer.add_scalar("Loss/Discriminator", D_loss, epoch)
+            writer.add_scalar("Loss/Generator", G_loss, epoch)
         
-    if config.SAVE_MODEL and epoch % 5 == 0:
-        save_checkpoint(generator, optim_gen, config.GEN_CHECKPOINT_PATH)
-        save_checkpoint(discriminator, optim_desc, config.DESC_CHECKPOINT_PATH)
+        if config.SAVE_MODEL and epoch % 5 == 0:
+            save_checkpoint(generator, optim_gen, config.GEN_CHECKPOINT_PATH)
+            save_checkpoint(discriminator, optim_desc, config.DESC_CHECKPOINT_PATH)
         
-    save_some_samples(generator, val_loader, epoch, folder='eval_files')
+        save_some_samples(generator, val_loader, epoch, folder='eval_files')
         
         
 if __name__ == "__main__":
